ceb46f3eb7082ffabc8a1ad54e1a0e4b0b84a665
Mark Strempel <mark.strempel@codecool.com>
2023-02-27 14:43:03 +0100

    Added functionality for running simulations.

--- simulation.js
+++ simulation.js
@@ -23,28 +23,64 @@ function randomNumber(min, max) {
     return Math.random() * (max - min) + min
 }
 
-const simulation = {
+let simulation = {
     particles: createRandomParticles({ numberOfParticles: 6,
         position: { x: { min: 0, max: 500}, y: { min: 0, max: 500 }}, 
         velocity: { x: { min: -10, max: 10}, y: { min: -10, max: 10 }}})
 };
 
-function showSimulation(canvas, simulation) {
-    canvas.restore();
+function simulateOneStep(simulation, timeDelta) {
+    const newParticles = []
 
     for (const particle of simulation.particles) {
         const { x, y } = particle.position;
+        const { x: xVelocity, y: yVelocity } = particle.velocity;
+
+        const newX = x + xVelocity * timeDelta;
+        const newY = y + yVelocity * timeDelta;
+
+        newParticles.push({
+            position: { x: newX, y: newY },
+            velocity: { x: xVelocity, y: yVelocity }
+        });
+    }
+
+    return {
+        particles: newParticles
+    }
+}
+
+function showSimulation(canvas, simulation) {
+    for (const particle of simulation.particles) {
+        const { x, y } = particle.position;
+        canvas.beginPath();
         canvas.moveTo(x, y);
         canvas.ellipse(x, y, 5, 5, 0, 0, 360);
         canvas.fill();
     }
-
-    canvas.save();
 }
 
 const startButton = document.querySelector("#start-button");
 const stopButton = document.querySelector("#stop-button");
 const simulationCanvas = document.querySelector("#simulation-canvas");
 const canvasContext = simulationCanvas.getContext("2d");
-
 showSimulation(canvasContext, simulation);
+
+let isRunning = false;
+startButton.addEventListener("click", function(event) {
+    const simulate = function() {
+        simulation = simulateOneStep(simulation, 60 / 1000);
+        canvasContext.clearRect(0, 0, simulationCanvas.width, simulationCanvas.height);
+        showSimulation(canvasContext, simulation);
+        if (isRunning) {
+            setTimeout(simulate, 1000 / 60);
+        }
+    }
+
+    isRunning = true;
+    simulate();
+})
+
+stopButton.addEventListener("click", function(event) {
+    isRunning = false;
+})
