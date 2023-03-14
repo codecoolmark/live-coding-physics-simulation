961f13e08c4d983cd59390feff18e12df4d45c21
Mark Strempel <mark.strempel@codecool.com>
2023-03-14 09:56:11 +0100

    Finished initSimulation function and implemented simulateOneStep function.

--- simulation.js
+++ simulation.js
@@ -16,7 +16,39 @@ const simulation = {
     ]
 }
 
-function startSimulation() {}
+// this functions get a simulation frame 
+// and returns the next frame
+function simulateOneStep(simulation) {
+    const newSimulation = {};
+    const newParticles = [];
+
+    for (const particle of simulation.particles) {
+        const currentPosition = particle.position;
+        const currentVelocity = particle.velocity;
+        
+        const newPosition = {};
+        newPosition.x = currentPosition.x + currentVelocity.x;
+        newPosition.y = currentPosition.y + currentVelocity.y;
+
+        const newParticle = { ...particle };
+        newParticle.position = newPosition;
+
+        newParticles.push(newParticle);
+    }
+
+    newSimulation.particles = newParticles;
+
+    return newSimulation
+}
+
+function startSimulation() {
+    const previousSimulation = initSimulation(1);
+    console.log("before one step", previousSimulation.particles);
+    nextSimulation = simulateOneStep(previousSimulation)
+    console.log("after one step", nextSimulation.particles);
+    // todo user interface (board, buttons, help)
+    // simulation loop
+}
 
 function stopSimulation() {}
 
@@ -52,12 +84,18 @@ function createParticle({ boxWidth = 100,
     return newParticle;
 }
 
-console.log(createParticle({
-    minDiameter: 10, maxDiameter: 20
-}));
-
-function initSimulation() {
+function initSimulation(numberOfParticles, particleOptions = {}) {
     const simulation = {};
+
+    const particles = [];
+    for (let particleNumber = 0; particleNumber < numberOfParticles; particleNumber++) {
+        const newParticle = createParticle(particleOptions);
+        particles.push(newParticle);
+    }
     
+    simulation.particles = particles;
+
     return simulation;
 }
+
+startSimulation();
